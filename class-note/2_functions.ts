ae35c49dd8fbaf553f6cdfaf5d4c88ed84d03b30
hyerimhan <hyerim_han@naver.com>
2022-05-04 02:36:48 +0900

    functions

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -14,7 +14,7 @@ function add(): number {
 function sum(a: number, b: number): number {
     return a + b;
 }
-sum(10);
+sum(10); // 에러. sum(10, 20);로 정의해야함.
 
 // 함수의 선택적 파라미터(옵셔널 파라미터) - 물음표
 function log(a: string, b?: string) {
