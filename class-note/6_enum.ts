76ace529f6b0bc37e3fca17872302d958984d2e4
hyerimhan <hyerim_han@naver.com>
2022-05-06 22:09:48 +0900

    이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,28 +1,48 @@
+// 숫자형 이넘
+// enum Shoes {
+//   Nike = 10, //10
+//   Adidas // 11 (처음 정의된 값에서 1씩 증가함)
+// }
+
+// var myShoes = Shoes.Nike;
+// console.log(myShoes); // 정의가 되어있지 않으면 0
+
+// 문자형 이넘
 enum Shoes {
-    Nike = '나이키',
-    Adidas = '아디다스',
-}
-
-var myShoes = Shoes.Nike;
-console.log(myShoes); // '나이키'
-
-// 예제
-enum Answer {
-    Yes = 'Y',
-    No = 'N',
-}
-
-function askQuestion(answer: Answer) {
-    if(answer == Answer.Yes) {
-        console.log('정답입니다.');
+    Nike = "나이키",
+    Adidas = "아디다스"
+  }
+  var myShoes = Shoes.Nike;
+  console.log(myShoes); // '나이키'
+  
+  // 이넘 활용 사례 (구체적인 값 정하기)
+  enum Answer {
+    Yes = "Y",
+    No = "N"
+  }
+  
+  // function askQuestion(answer: string) {
+  //   if (answer === "yes") {
+  //     console.log("정답입니다.");
+  //   }
+  //   if (answer === "no") {
+  //     console.log("오답입니다.");
+  //   }
+  // }
+  // askQuestion("예스"); // O
+  // askQuestion("y"); // O
+  // askQuestion("Yes"); // O
+  
+  function askQuestion(answer: Answer) {
+    if (answer === Answer.Yes) {
+      console.log("정답입니다.");
     }
-    if(answer == Answer.No) {
-        console.log('오답입니다.');
+    if (answer === Answer.No) {
+      console.log("오답입니다.");
     }
-}
-
-askQuestion(Answer.Yes); // O
-// askQuestion('Yes'); // X
-// askQuestion('예스'); // X
-// askQuestion('y'); // X
-// askQuestion('Yes'); // X
+  }
+  askQuestion(Answer.Yes); // O
+  askQuestion("예스"); // X
+  askQuestion("y"); // X
+  askQuestion("Yes"); // X
+  
