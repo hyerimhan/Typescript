91a345bdddc291c050dc8bff4909c5bb7b016572
hyerimhan <hyerim_han@naver.com>
2022-05-06 22:08:36 +0900

    유니온과 인터섹션

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -1,48 +1,50 @@
+// * 연산자를 이용한 타입 정의 - Union Type
 // function logMessage(value: any) {
-//     console.log(value);
+//   console.log(value);
 // }
 // logMessage('hello');
 // logMessage(100);
 
-// 유니온타입(=or)
-var sehp: string | number | boolean;
+var seho: string | number | boolean;
 function logMessage(value: string | number) {
-    if (typeof value == 'number') {
-        value.toLocaleString();
-    }
-    if(typeof value == 'string') {
-        value.toString();
-    }
-    throw new TypeError('value must be string or number');
+  if (typeof value === "number") {
+    value.toLocaleString();
+  }
+  if (typeof value === "string") {
+    value.toString();
+  }
+  throw new TypeError("value must be string or number");
 }
-logMessage('hello');
+logMessage("hello");
 logMessage(100);
 
-interface Developer {
-    name: string;
-    skill: string;
+// --------------------------------------------------------
+interface Devloper {
+  name: string;
+  skill: string;
 }
 
 interface Person {
-    name: string;
-    age: number;
+  name: string;
+  age: number;
 }
 
-// function askSomeone(someone: Developer | Person) {
-//     someone.name; // O
-//     // someone.skill // X
-//     // someone.age // X
+// function askSomeone(someone: Devloper | Person) {
+//   // someone.name; // O
+//   // someone.skill; // X
+//   // someone.age; // X
 // }
-// askSomeone({ name: '디벨로퍼', skill: '웹개발' });
-// askSomeone({ name: '캡틴', age: 100 });
+// // 데이터를 따라서 따로따로 넘기기
+// askSomeone({ name: "디벨로퍼", skill: "웹 개발" });
+// askSomeone({ name: "캡틴", age: 100 });
 
-// 인터섹션 (=and)
-function askSomeone(someone: Developer & Person) {
-    someone.name; // O
-    someone.skill // O
-    someone.age // O
+// * 연산자를 이용한 타입 정의 - Intersection Type
+// var seho: string | number | boolean;
+// var capt: string & number & boolean; // var capt: never
+
+function askSomeone(someone: Devloper & Person) {
+  someone.name; // O
+  someone.skill; // O
+  someone.age; // O
 }
-askSomeone({ name: '디벨로퍼', skill: '웹개발', age: 34 });
-
-var seho: string | number | boolean;
-var capt: string & number & boolean;
+askSomeone({ name: "디벨로퍼", skill: "웹 개발", age: 34 }); // age도 설정해야함.
