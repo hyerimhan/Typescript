6225774ac9df14a2313e42e2045c4a0240f0fb38
hyerimhan <hyerim_han@naver.com>
2022-05-06 23:39:49 +0900

    클래스

--- class-note/class.js
+++ class-note/class.js
@@ -1,11 +1,52 @@
-class Person {
-    // 클래스 로직
-    // 초기화
-    constructor(name, age) {
-        console.log('생성 되었습니다');
-        this.name = name;
-        this.age = age;
-    }
-}
+// 클래스는 ES2015 (E6)부터 소개된 최신 문법
 
-var seho = new Person('세호', 30); // 생성 되었습니다.
+// class Person {
+//   // 클래스 로직
+//   // 초기화 메서드
+//   constructor(name, age) {
+//     console.log("생성 되었습니다.");
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// // 생성 되었습니다.
+// var seho = new Person("세호", 30);
+// console.log(seho);
+
+// 자바스크립트의 프로토타입 (중복되는 값 상속하기)
+var user = { name: "capt", age: 100 };
+var admin = { name: "capt", age: 100, role: "admin" };
+
+var admin = {};
+admin.__proto__ = user; // admin에 user내용 상속받기
+
+// 프로토타입 활용 사례
+var obj = { a: 10 };
+Object.keys(obj); // ["a"]
+obj.hasOwnProperty("a"); // true
+
+var user = {name: 'capt', age: 100};
+var arr = [];
+arr. // .다음은 기본 내장되어 있는 프로토타입 기본문법을 볼 수 있음.
+
+// 프로토타입과 클래스와의 관계 (자바나 객체 지향언어가 익숙한 개발자들이 생성자 함수(function)을 클래스를 만들어 냄.)
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+// }
+// var capt = new Person('캡틴', 100);
+// // 위와 아래는 같은 코드
+// class Person {
+//   // 클래스 로직
+//   // 초기화 메서드
+//   constructor(name, age) {
+//     console.log("생성 되었습니다.");
+//     this.name = name;
+//     this.age = age;
+//   }
+// }
+
+// // 생성 되었습니다.
+// var seho = new Person("세호", 30);
+// console.log(seho);
