f22110d4a0540ed9331219312eac3220956659d8
hyerimhan <hyerim_han@naver.com>
2022-05-09 00:39:40 +0900

    타입 단언

--- class-note/10_type-assertion.ts
+++ class-note/10_type-assertion.ts
@@ -6,4 +6,5 @@ var b = a as string; // 전 : any 후 : string
 
 // DOM API 조작
 // <div id="app">hi</div>
-var div = document.querySelector("div") as HTMLDivElement; // document에 div Element가 있을것이다. 라고 단언하기
+// var div: HTMLDivElement | null
+var div = document.querySelector("div") as HTMLDivElement; // document에 div Element(HTMLDivElement)가 있을것이다. 라고 단언하기
