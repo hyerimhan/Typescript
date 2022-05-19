df2799323585d6e0d77cde01211ac6a709c2fb5f
hyerimhan <hyerim_han@naver.com>
2022-05-20 01:03:05 +0900

    국가별 코로나 정보 API 함수 타입

--- project/src/covid/index.ts
+++ project/src/covid/index.ts
@@ -28,3 +28,18 @@ export interface CovidSummaryResponse {
   Global: Global;
   message: string;
 }
+
+interface CountrySummaryInfo {
+  Cases: number;
+  City: string;
+  CityCode: string;
+  Country: string;
+  CountryCode: string;
+  Date: string;
+  Lat: string;
+  Lon: string;
+  Province: string;
+  Status: string;
+}
+
+export type CountrySummaryResponse = CountrySummaryInfo[];
